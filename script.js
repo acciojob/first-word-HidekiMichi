6433a4e551961371d9c82ddce7ef0972367761f6
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2024-01-30 11:17:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	if(s==''){
+		return '';
+	}
+	s=s.trim();
+	let arr=s.split(' ');
+	return arr[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s));  
